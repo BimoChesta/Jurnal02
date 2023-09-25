9ac031b3f86397c9685af362dafdacf80187b853
BimoChesta <bimo74755@gmail.com>
2023-09-25 15:31:35 +0700

    first commit

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01{
+    public static void main(String[] args){
+        System.out.println("Nama : Bimo Chesta Adabi");
+        System.out.println("Nim : 607062300064");
+        System.out.println("Kelas : D3IF-47-03");
+    }
+}
